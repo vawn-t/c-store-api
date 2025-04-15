f562bd1c80782aadd4f186984f61fccd30e38d21
vawn <vantran99dn@gmail.com>
2025-04-15 23:56:48 +0700

    Add Passport authentication strategies and Prisma client setup; update dependencies

---
+++ src/middlewares/passport.middleware.ts
@@ -0,0 +1,62 @@
+// Libraries
+import Passport from 'passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+// Prisma
+import { UserStatus } from '@prisma/client';
+import prisma from '../prisma';
+
+const middlewarePassportStrategy = (passport: typeof Passport) => {
+  // Config strategy for authentication token
+  passport.use(
+    'jwt-auth',
+    new Strategy(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+        secretOrKey: process.env.JWT_AUTHENTICATE_SECRET,
+      },
+      async (payload, done) => {
+        try {
+          const user = await prisma.user.findFirst({
+            where: { id: payload.userId },
+          });
+
+          if (!!user && user.status === UserStatus.ACTIVE) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        } catch (error) {
+          done(error);
+        }
+      },
+    ),
+  );
+  // Config strategy for verify otp token
+  passport.use(
+    'jwt-verify-otp',
+    new Strategy(
+      {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+        secretOrKey: process.env.JWT_AUTHENTICATE_OTP_SECRET,
+      },
+      async (payload, done) => {
+        try {
+          const user = await prisma.user.findFirst({
+            where: { id: payload.userId },
+          });
+
+          if (!!user && user.status === UserStatus.PRE_ACTIVE) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        } catch (error) {
+          done(error);
+        }
+      },
+    ),
+  );
+};
+
+export { middlewarePassportStrategy };
